c3a4067b6266a6012a21585fcbb14fa690843e50
mslmyrtd <mslmyrtd@gmail.com>
2023-03-01 00:54:47 +0300

    cardContent comp created structure edited

---
+++ src/components/CardContent.js
@@ -0,0 +1,111 @@
+import React from 'react'
+
+const CardContent = () => {
+  return (
+    <>
+      {' '}
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+      <div class='max-w-sm rounded overflow-hidden  hover:shadow-lg cursor-pointer transition duration-700 ease-in-out'>
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>The Coldest Sunset</div>
+          <p class='text-gray-700 text-base'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CardContent
