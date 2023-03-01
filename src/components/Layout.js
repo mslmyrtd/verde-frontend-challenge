534697abfae4a6e8a48d860aede5cac137580741
mslmyrtd <mslmyrtd@gmail.com>
2023-03-01 13:04:10 +0300

    edited

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Layout = ({ children }) => {
   return (
-    <div class='overflow-x-hidden bg-white p-11 m-7 rounded-md'>{children}</div>
+    <div className='overflow-x-hidden bg-white p-11 m-7 rounded-md'>
+      {children}
+    </div>
   )
 }
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,7 +4,7 @@ import PostContainer from '../components/PostContainer'
 const Dashboard = () => {
   return (
     <div>
-      <div class='grid lg:grid-cols-3 gap-2 m-2 md:grid-cols-2 sm:grid-cols-1'>
+      <div className='grid lg:grid-cols-3 gap-2 m-2 md:grid-cols-2 sm:grid-cols-1'>
         <PostContainer />
       </div>
     </div>
